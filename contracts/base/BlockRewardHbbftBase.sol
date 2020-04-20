3ac158e0525b1c053108d3e690fccc66b85a6ee2
i-stam <i.stamelakos@gmail.com>
2020-04-21 00:37:03 +0100

    Remove bridge related functions from BlockReward contract

--- contracts/base/BlockRewardHbbftBase.sol
+++ contracts/base/BlockRewardHbbftBase.sol
@@ -25,17 +25,6 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
     // existing storage variables and do not change their types!
 
     mapping(address => uint256[]) internal _epochsPoolGotRewardFor;
-    mapping(address => bool) internal _ercToNativeBridgeAllowed;
-    address[] internal _ercToNativeBridgesAllowed;
-    bool internal _queueERInitialized;
-    uint256 internal _queueERFirst;
-    uint256 internal _queueERLast;
-    struct ExtraReceiverQueue {
-        uint256 amount;
-        address bridge;
-        address receiver;
-    }
-    mapping(uint256 => ExtraReceiverQueue) internal _queueER;
 
     /// @dev A number of blocks produced by the specified validator during the specified staking epoch
     /// (beginning from the block when the `finalizeChange` function is called until the latest block
@@ -44,33 +33,9 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
     /// While the validator is banned, the block producing statistics is not accumulated for them.
     mapping(uint256 => mapping(address => uint256)) public blocksCreated;
 
-    /// @dev The current bridge's total fee amount of native coins accumulated by
-    /// the `addBridgeNativeFeeReceivers` function.
-    uint256 public bridgeNativeFee;
-
     /// @dev The reward amount to be distributed in native coins among participants (the validator and their
     /// delegators) of the specified pool (mining address) for the specified staking epoch.
     mapping(uint256 => mapping(address => uint256)) public epochPoolNativeReward;
-
-    /// @dev The total amount of native coins minted for the specified address
-    /// by the `erc-to-native` bridges through the `addExtraReceiver` function.
-    mapping(address => uint256) public mintedForAccount;
-
-    /// @dev The amount of native coins minted at the specified block for the specified
-    /// address by the `erc-to-native` bridges through the `addExtraReceiver` function.
-    mapping(address => mapping(uint256 => uint256)) public mintedForAccountInBlock;
-
-    /// @dev The total amount of native coins minted at the specified block
-    /// by the `erc-to-native` bridges through the `addExtraReceiver` function.
-    mapping(uint256 => uint256) public mintedInBlock;
-
-    /// @dev The total amount of native coins minted by the
-    /// `erc-to-native` bridges through the `addExtraReceiver` function.
-    uint256 public mintedTotally;
-
-    /// @dev The total amount of native coins minted by the specified
-    /// `erc-to-native` bridge through the `addExtraReceiver` function.
-    mapping(address => uint256) public mintedTotallyByBridge;
 
     /// @dev The total reward amount in native coins which is not yet distributed among pools.
     uint256 public nativeRewardUndistributed;
@@ -95,36 +60,8 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
 
     // ================================================ Events ========================================================
 
-    /// @dev Emitted by the `addExtraReceiver` function.
-    /// @param amount The amount of native coins which must be minted for the `receiver` by the `erc-to-native`
-    /// `bridge` with the `reward` function.
-    /// @param receiver The address for which the `amount` of native coins must be minted.
-    /// @param bridge The bridge address which called the `addExtraReceiver` function.
-    event AddedReceiver(uint256 amount, address indexed receiver, address indexed bridge);
-
-    /// @dev Emitted by the `addBridgeNativeFeeReceivers` function.
-    /// @param amount The fee amount in native coins passed to the
-    /// `addBridgeNativeFeeReceivers` function as a parameter.
-    /// @param cumulativeAmount The value of `bridgeNativeFee` state variable
-    /// after adding the `amount` to it.
-    /// @param bridge The bridge address which called the `addBridgeNativeFeeReceivers` function.
-    event BridgeNativeFeeAdded(uint256 amount, uint256 cumulativeAmount, address indexed bridge);
-
-    /// @dev Emitted by the `_mintNativeCoins` function which is called by the `reward` function.
-    /// This event is only used by the unit tests because the `reward` function cannot emit events.
-    /// @param receivers The array of receiver addresses for which native coins are minted. The length of this
-    /// array is equal to the length of the `rewards` array.
-    /// @param rewards The array of amounts minted for the relevant `receivers`. The length of this array
-    /// is equal to the length of the `receivers` array.
-    event MintedNative(address[] receivers, uint256[] rewards);
 
     // ============================================== Modifiers =======================================================
-
-    /// @dev Ensures the caller is the `erc-to-native` bridge contract address.
-    modifier onlyErcToNativeBridge {
-        require(_ercToNativeBridgeAllowed[msg.sender]);
-        _;
-    }
 
     /// @dev Ensures the `initialize` function was called before.
     modifier onlyInitialized {
@@ -157,27 +94,6 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
         revert();
     }
 
-    /// @dev Called by the `erc-to-native` bridge contract when a portion of the bridge fee should be minted
-    /// and distributed to participants (validators and their delegators) in native coins. The specified amount
-    /// is used by the `_distributeRewards` function.
-    /// @param _amount The fee amount distributed to participants.
-    function addBridgeNativeFeeReceivers(uint256 _amount) external onlyErcToNativeBridge {
-        require(_amount != 0);
-        bridgeNativeFee = bridgeNativeFee.add(_amount);
-        emit BridgeNativeFeeAdded(_amount, bridgeNativeFee, msg.sender);
-    }
-
-    /// @dev Called by the `erc-to-native` bridge contract when the bridge needs to mint a specified amount of native
-    /// coins for a specified address using the `reward` function.
-    /// @param _amount The amount of native coins which must be minted for the `_receiver` address.
-    /// @param _receiver The address for which the `_amount` of native coins must be minted.
-    function addExtraReceiver(uint256 _amount, address _receiver) external onlyErcToNativeBridge {
-        require(_amount != 0);
-        require(_queueERInitialized);
-        _enqueueExtraReceiver(_amount, _receiver, msg.sender);
-        emit AddedReceiver(_amount, _receiver, msg.sender);
-    }
-
     /// @dev Called by the `ValidatorSetHbbft.finalizeChange` to clear the values in
     /// the `blocksCreated` mapping for the current staking epoch and a new validator set.
     function clearBlocksCreated() external onlyValidatorSetContract {
@@ -200,27 +116,11 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
         validatorMinRewardPercent[0] = VALIDATOR_MIN_REWARD_PERCENT;
     }
 
-    /// @dev Copies the minting statistics from the previous BlockReward contract
-    /// for the `mintedTotally` and `mintedTotallyByBridge` getters. Can only be called once by the owner.
-    /// This function assumes the bridge contract address is not changed due to its upgradable nature.
-    /// @param _bridge The address of a bridge contract.
-    /// @param _prevBlockRewardContract The address of the previous BlockReward contract.
-    function migrateMintingStatistics(address _bridge, IBlockRewardHbbft _prevBlockRewardContract) external onlyOwner {
-        require(mintedTotally == 0);
-        uint256 prevMinted = _prevBlockRewardContract.mintedTotally();
-        uint256 prevMintedByBridge = _prevBlockRewardContract.mintedTotallyByBridge(_bridge);
-        require(prevMinted != 0);
-        require(prevMintedByBridge != 0);
-        mintedTotally = prevMinted;
-        mintedTotallyByBridge[_bridge] = prevMintedByBridge;
-    }
-
     /// @dev Called by the validator's node when producing and closing a block,
     /// see https://wiki.parity.io/Block-Reward-Contract.html.
     /// This function performs all of the automatic operations needed for accumulating block producing statistics,
     /// starting a new staking epoch, snapshotting staking amounts for the upcoming staking epoch,
-    /// rewards distributing at the end of a staking epoch, 
-    /// and minting native coins needed for the `erc-to-native` bridge.
+    /// and rewards distributing at the end of a staking epoch.
     function reward(address[] calldata benefactors, uint16[] calldata kind)
         external
         onlySystem
@@ -235,14 +135,6 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
             return (new address[](0), new uint256[](0));
         }
 
-        // Initialize the extra receivers queue
-        if (!_queueERInitialized) {
-            _queueERFirst = 1;
-            _queueERLast = 0;
-            _queueERInitialized = true;
-        }
-
-        uint256 bridgeQueueLimit = 100;
         IStakingHbbft stakingContract = IStakingHbbft(validatorSetContract.stakingContract());
         uint256 stakingEpoch = stakingContract.stakingEpoch();
         uint256 stakingFixedEpochEndBlock = stakingContract.stakingFixedEpochEndBlock();
@@ -295,38 +187,11 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
             // Remember validator's min reward percent for the upcoming staking epoch
             validatorMinRewardPercent[nextStakingEpoch] = VALIDATOR_MIN_REWARD_PERCENT;
 
-            // Pause bridge for this block
-            bridgeQueueLimit = 0;
         }
 
-        // Mint native coins if needed
-        return _mintNativeCoins(nativeTotalRewardAmount, bridgeQueueLimit);
-    }
-
-    /// @dev Sets the array of `erc-to-native` bridge addresses which are allowed to call some of the functions with
-    /// the `onlyErcToNativeBridge` modifier. This setter can only be called by the `owner`.
-    /// @param _bridgesAllowed The array of bridge addresses.
-    function setErcToNativeBridgesAllowed(address[] calldata _bridgesAllowed) external onlyOwner onlyInitialized {
-        uint256 i;
-
-        for (i = 0; i < _ercToNativeBridgesAllowed.length; i++) {
-            _ercToNativeBridgeAllowed[_ercToNativeBridgesAllowed[i]] = false;
-        }
-
-        _ercToNativeBridgesAllowed = _bridgesAllowed;
-
-        for (i = 0; i < _bridgesAllowed.length; i++) {
-            _ercToNativeBridgeAllowed[_bridgesAllowed[i]] = true;
-        }
     }
 
     // =============================================== Getters ========================================================
-
-    /// @dev Returns an identifier for the bridge contract so that the latter could
-    /// ensure it works with the BlockReward contract.
-    function blockRewardContractId() public pure returns(bytes4) {
-        return 0x0d35a7ca; // bytes4(keccak256("blockReward"))
-    }
 
     /// @dev Returns an array of epoch numbers for which the specified pool (mining address)
     /// got a non-zero reward.
@@ -334,25 +199,9 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
         return _epochsPoolGotRewardFor[_miningAddress];
     }
 
-    /// @dev Returns the array of `erc-to-native` bridge addresses set by the `setErcToNativeBridgesAllowed` setter.
-    function ercToNativeBridgesAllowed() public view returns(address[] memory) {
-        return _ercToNativeBridgesAllowed;
-    }
-
-    /// @dev Returns the current size of the address queue created by the `addExtraReceiver` function.
-    function extraReceiversQueueSize() public view returns(uint256) {
-        return _queueERLast + 1 - _queueERFirst;
-    }
-
     /// @dev Returns a boolean flag indicating if the `initialize` function has been called.
     function isInitialized() public view returns(bool) {
         return validatorSetContract != IValidatorSetHbbft(0);
-    }
-
-    /// @dev Prevents sending tokens directly to the `BlockRewardHbbft` contract address
-    /// by the `ERC677BridgeTokenRewardable.transferAndCall` function.
-    function onTokenTransfer(address, uint256, bytes memory) public pure returns(bool) {
-        revert();
     }
 
     /// @dev Returns an array of epoch numbers for which the specified staker
@@ -559,13 +408,11 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
         uint256[] memory _blocksCreatedShareNum,
         uint256 _blocksCreatedShareDenom
     ) internal returns(uint256) {
-        uint256 totalReward = bridgeNativeFee + nativeRewardUndistributed;
+        uint256 totalReward = nativeRewardUndistributed;
 
         if (totalReward == 0) {
             return 0;
         }
-
-        bridgeNativeFee = 0;
 
         uint256 rewardToDistribute = 0;
         uint256 distributedAmount = 0;
@@ -666,101 +513,6 @@ contract BlockRewardHbbftBase is UpgradeableOwned, IBlockRewardHbbft {
     /// @dev Returns the current block number. Needed mostly for unit tests.
     function _getCurrentBlockNumber() internal view returns(uint256) {
         return block.number;
-    }
-
-    /// @dev Joins two native coin receiver elements into a single set and returns the result
-    /// to the `reward` function: the first element comes from the `erc-to-native` bridge fee distribution,
-    /// the second - from the `erc-to-native` bridge when native coins are minted for the specified addresses.
-    /// Dequeues the addresses enqueued with the `addExtraReceiver` function by the `erc-to-native` bridge.
-    /// Accumulates minting statistics for the `erc-to-native` bridges.
-    /// @param _nativeTotalRewardAmount The native coins amount which should be accrued to the balance
-    /// of this contract (as a total reward for the finished staking epoch).
-    /// @param _queueLimit Max number of addresses which can be dequeued from the queue formed by the
-    /// `addExtraReceiver` function.
-    function _mintNativeCoins(
-        uint256 _nativeTotalRewardAmount,
-        uint256 _queueLimit
-    )
-        internal
-        returns(address[] memory receivers, uint256[] memory rewards)
-    {
-        uint256 extraLength = extraReceiversQueueSize();
-
-        if (extraLength > _queueLimit) {
-            extraLength = _queueLimit;
-        }
-
-        bool totalRewardNotEmpty = _nativeTotalRewardAmount != 0;
-
-        receivers = new address[](extraLength + (totalRewardNotEmpty ? 1 : 0));
-        rewards = new uint256[](receivers.length);
-
-        for (uint256 i = 0; i < extraLength; i++) {
-            (uint256 amount, address receiver, address bridge) = _dequeueExtraReceiver();
-            receivers[i] = receiver;
-            rewards[i] = amount;
-            _setMinted(amount, receiver, bridge);
-        }
-
-        if (totalRewardNotEmpty) {
-            receivers[extraLength] = address(this);
-            rewards[extraLength] = _nativeTotalRewardAmount;
-        }
-
-        emit MintedNative(receivers, rewards);
-
-        return (receivers, rewards);
-    }
-
-    /// @dev Dequeues the information about the native coins receiver enqueued with the `addExtraReceiver`
-    /// function by the `erc-to-native` bridge. This function is used by `_mintNativeCoins`.
-    /// @return `uint256 amount` - The amount to be minted for the `receiver` address.
-    /// `address receiver` - The address for which the `amount` is minted.
-    /// `address bridge` - The address of the bridge contract which called the `addExtraReceiver` function.
-    function _dequeueExtraReceiver() internal returns(uint256 amount, address receiver, address bridge) {
-        uint256 queueFirst = _queueERFirst;
-        uint256 queueLast = _queueERLast;
-
-        if (queueLast < queueFirst) {
-            amount = 0;
-            receiver = address(0);
-            bridge = address(0);
-        } else {
-            amount = _queueER[queueFirst].amount;
-            receiver = _queueER[queueFirst].receiver;
-            bridge = _queueER[queueFirst].bridge;
-            delete _queueER[queueFirst];
-            _queueERFirst++;
-        }
-    }
-
-    /// @dev Enqueues the information about the receiver of native coins which must be minted for the
-    /// specified `erc-to-native` bridge. This function is used by the `addExtraReceiver` function.
-    /// @param _amount The amount of native coins which must be minted for the `_receiver` address.
-    /// @param _receiver The address for which the `_amount` of native coins must be minted.
-    /// @param _bridge The address of the bridge contract which requested the minting of native coins.
-    function _enqueueExtraReceiver(uint256 _amount, address _receiver, address _bridge) internal {
-        uint256 queueLast = _queueERLast + 1;
-        _queueER[queueLast] = ExtraReceiverQueue({
-            amount: _amount,
-            bridge: _bridge,
-            receiver: _receiver
-        });
-        _queueERLast = queueLast;
-    }
-
-    /// @dev Accumulates minting statistics for the `erc-to-native` bridge.
-    /// This function is used by the `_mintNativeCoins` function.
-    /// @param _amount The amount minted for the `_account` address.
-    /// @param _account The address for which the `_amount` is minted.
-    /// @param _bridge The address of the bridge contract which called the `addExtraReceiver` function.
-    function _setMinted(uint256 _amount, address _account, address _bridge) internal {
-        uint256 blockNumber = _getCurrentBlockNumber();
-        mintedForAccountInBlock[_account][blockNumber] = _amount;
-        mintedForAccount[_account] += _amount;
-        mintedInBlock[blockNumber] += _amount;
-        mintedTotallyByBridge[_bridge] += _amount;
-        mintedTotally += _amount;
     }
 
     /// @dev Makes snapshots of total amount staked into the specified pool
