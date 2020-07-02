17504c641f69c38c9f8b9921783136039d695e28
David Forstenlechner <dforsten@gmail.com>
2020-07-02 15:08:36 +0200

    Deployed KeygenHistory to a new Truffle Unit Test file.
    Added a bash script for convenience to execute specifically the new Unit Test.

---
+++ run_validatorset_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+npm test test/ValidatorSetSimplified.js test/mockContracts/BlockRewardHbbftCoinsMock.sol test/mockContracts/RandomHbbftMock.sol test/mockContracts/ValidatorSetHbbftMock.sol test/mockContracts/StakingHbbftCoinsMock.sol
