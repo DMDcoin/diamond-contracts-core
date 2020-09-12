cff09e5c92580542b063b8fb723b4510d9eb224d
Thomas Haller <thomashaller@gmx.at>
2020-09-12 17:31:14 +0200

    correct time move forward for "should move entire stake" #12

--- test/StakingHbbft.js
+++ test/StakingHbbft.js
@@ -1789,7 +1789,10 @@ contract('StakingHbbft', async accounts => {
     });
 
     it('should move entire stake', async () => {
-      await callReward(true);
+
+      await timeTravelToTransition();
+      await timeTravelToEndEpoch();
+
       (await stakingHbbft.stakeAmount.call(initialStakingAddresses[0], delegatorAddress)).should.be.bignumber.equal(stakeAmount);
       (await stakingHbbft.stakeAmount.call(initialStakingAddresses[1], delegatorAddress)).should.be.bignumber.equal(new BN(0));
       await stakingHbbft.moveStake(initialStakingAddresses[0], initialStakingAddresses[1], stakeAmount, {from: delegatorAddress}).should.be.fulfilled;
