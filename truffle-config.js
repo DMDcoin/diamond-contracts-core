0e3b24d03b90b1559947210a51e63e8870ccf924
Thomas Haller <thomashaller@gmx.at>
2020-09-17 17:25:17 +0200

    config cleanup and switched to correct port for tests

--- truffle-config.js
+++ truffle-config.js
@@ -63,7 +63,7 @@ module.exports = {
     },
 
     test: {
-      provider: () => new HDWalletProvider(mnemonic, `http://127.0.0.1:8545`, 0, 200),
+      provider: () => new HDWalletProvider(mnemonic, `http://127.0.0.1:8544`, 0, 200),
       gas: 8000000,
       network_id: "*" // Match any network id
     },
@@ -75,34 +75,6 @@ module.exports = {
       gasPrice: 0x01,
       network_id: "*"
     },
-
-    // Another network with more advanced options...
-    advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },
-
-    // Useful for private networks
-    private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -113,7 +85,8 @@ module.exports = {
     //   gasPrice: 1,
     //   showTimeSpent: true
     // },
-    enableTimeouts: false
+    enableTimeouts: false,
+    timout: 1000000
   },
 
   // Configure your compilers
