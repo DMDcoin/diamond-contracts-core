50334bb5cbad676dc3753932e6d1b6b8bf1bc8b7
Thomas Haller <Thomas.Haller@lab10.coop>
2021-10-06 21:40:14 +0200

    get function signatures now also shows ValidatorSetHbbft

--- scripts/getFunctionSignatures.js
+++ scripts/getFunctionSignatures.js
@@ -3,21 +3,28 @@ const web3 = require('web3');
 const Web3EthAbi = require('web3-eth-abi');
 const fs = require('fs');
 
-fullFile = fs.readFileSync("build/contracts/KeyGenHistory.json").toString().trim();
+const files = ['KeyGenHistory', 'ValidatorSetHbbft'];
 
-const contractObj = JSON.parse(fullFile);
-const ABI = contractObj.abi;
-//json.load(open('/path/to/your/truffle/workspace' + '/build/contracts/AdditionContract.json'))
+for(let i = 0; i < files.length; i++) {
+  const file = files[i];
+  console.log(` === ${file} === `);
+  fullFile = fs.readFileSync(`build/contracts/${file}.json`).toString().trim();
 
-// Concatenate function name with its param types
-const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
+  const contractObj = JSON.parse(fullFile);
+  const ABI = contractObj.abi;
+  //json.load(open('/path/to/your/truffle/workspace' + '/build/contracts/AdditionContract.json'))
 
-//let encodedFunctionSignature = f => Web3EthAbi.encodeFunctionSignature('adopt(uint256)');
-let encodedFunctionSignature = f => Web3EthAbi.encodeFunctionSignature(f);
+  // Concatenate function name with its param types
+  const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
 
-// Parse ABI and encode its functions
-const output = ABI
-  .filter(e => e.type === "function")
-  .flatMap(e => `${encodedFunctionSignature(prepareData(e))}: ${prepareData(e)}`)
+  //let encodedFunctionSignature = f => Web3EthAbi.encodeFunctionSignature('adopt(uint256)');
+  let encodedFunctionSignature = f => Web3EthAbi.encodeFunctionSignature(f);
 
-console.log(output)
+  // Parse ABI and encode its functions
+  const output = ABI
+    .filter(e => e.type === "function")
+    .flatMap(e => `${encodedFunctionSignature(prepareData(e))}: ${prepareData(e)}`)
+
+  console.log(output)
+
+}
