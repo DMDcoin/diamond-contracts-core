a19564fe0f9e229a573313451f4a4980d1736d37
Kotsin <kdo5@mail.ru>
2022-10-19 00:47:50 +0700

    removed outdated files

--- run_validatorset_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-npm test test/ValidatorSetSimplified.js test/mockContracts/BlockRewardHbbftCoinsMock.sol test/mockContracts/RandomHbbftMock.sol test/mockContracts/ValidatorSetHbbftMock.sol test/mockContracts/StakingHbbftCoinsMock.sol
