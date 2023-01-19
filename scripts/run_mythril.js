ce4c99b9e12073195b339d9a13e48434d9defa07
Kotsin <kdo5@mail.ru>
2023-01-19 20:34:33 +0700

    fixed script

--- scripts/run_mythril.js
+++ scripts/run_mythril.js
@@ -4,36 +4,39 @@ const fs = require('fs');
 main();
 
 async function main() {
-    let dir = './contracts/';
+  let dir = './contracts/';
 
-    if (!fs.existsSync(dir)) {
-        dir = '.' + dir;
+  if (!fs.existsSync(dir)) {
+    dir = '.' + dir;
+  }
+
+  const filenames = fs.readdirSync(dir);
+  let contracts = [];
+
+  for (let i = 0; i < filenames.length; i++) {
+
+    if (filenames[i].endsWith('.sol')) {
+      contracts.push(filenames[i]);
     }
+  }
 
-    const filenames = fs.readdirSync(dir);
-    let contracts = [];
+  fs.mkdirSync("./audit", { recursive: true });
 
-    for (let i = 0; i < filenames.length; i++) {
+  for (let i = 0; i < contracts.length; i++) {
+    const promise = child.spawn('myth', [`analyze`, `./contracts/${[contracts[i]]}`]);
+    console.log(`${[contracts[i]]} audit have started`);
+    promise.stdout.on('data', (data) => {
+      fs.writeFile(`./audit/${[contracts[i].slice(0, -4)]}.txt`, data, function () { }
+      )
+      console.log(`results were written into the ./audit/${[contracts[i].slice(0, -4)]}.txt`)
+    });
 
-        if (filenames[i].endsWith('.sol')) {
-            contracts.push(filenames[i]);
-        }
-    }
+    promise.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
 
-    for (let i = 0; i < contracts.length; i++) {
-        const promise = child.spawn('myth', [`analyze`, `./contracts/${[contracts[i]]}`]);
-        console.log(`child ${[contracts[i]]} was spawned`);
-        promise.stdout.on('data', (data) => {
-            fs.writeFile(`./audit/${[contracts[i]]}.txt`, data, function () { }
-            )
-        });
-
-        promise.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-        });
-
-        promise.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
-    }
+    promise.on('close', (code) => {
+      console.log(`exited with code ${code}`);
+    });
+  }
 }
