64a4a66ee332bc9d6e86eec30d8fa1fbef5736c6
Thomas Haller <thomashaller@gmx.at>
2023-10-31 12:48:59 +0100

    switch transition window to 3 minutes

--- scripts/upgrade_contracts2.ts
+++ scripts/upgrade_contracts2.ts
@@ -1,16 +1,14 @@
 import { ethers, upgrades } from "hardhat";
 
+// second step of contract upgade:
+// setStakingTransitionTimeframeLength to 300 seconds. 
 
 async function upgrade() {
 
     const [deployer] = await ethers.getSigners();
-
     console.log("upgrading with account from: ", deployer.address);
-
     let stakingContract = await ethers.getContractAt("StakingHbbft", "0x1100000000000000000000000000000000000001");
-
-    let txResult = await stakingContract.setStakingTransitionTimeframeLength(300);
-
+    let txResult = await stakingContract.setStakingTransitionTimeframeLength(180);
     console.log("Done.", txResult);
 }
 
