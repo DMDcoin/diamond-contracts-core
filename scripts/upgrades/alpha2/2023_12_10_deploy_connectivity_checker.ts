f66aa815373dfd7405747945612ea55fdcb28255
Thomas Haller <thomashaller@gmx.at>
2023-12-09 23:02:49 +0100

    concept for the early epoch end upgrade

---
+++ scripts/upgrades/alpha2/2023_12_10_deploy_connectivity_checker.ts
@@ -0,0 +1,20 @@
+
+
+
+async function doUpgrade() {
+
+    // deploy connectivity checker
+    // deploy proxy for connectivity checker
+    
+    // initialize connectivity checker.
+
+    // upgrade contracts
+    // ------------
+    // - reward
+    // - permission
+    // - validator set
+    // - staking
+
+}
+
+doUpgrade();
