dd1ec97ce619793130466745f3f3c1ed3c55dc54
Thomas Haller <thomashaller@gmx.at>
2023-12-11 10:13:25 +0100

    WIP upgrade contracts

--- scripts/upgrades/alpha2/2023_12_10_deploy_connectivity_checker.ts
+++ scripts/upgrades/alpha2/2023_12_10_deploy_connectivity_checker.ts
@@ -1,12 +1,41 @@
-
+import { ethers, upgrades } from "hardhat";
+import { ConnectivityTrackerHbbft } from "../../../src/types/contracts/ConnectivityTrackerHbbft";
 
 
 async function doUpgrade() {
 
+    const [deployer] = await ethers.getSigners();
+    //console.log("upgrading with account from: ", deployer.address);
+    //let validatorSetHbbft = await ethers.getContractAt("ValidatorSetHbbft", "0x1000000000000000000000000000000000000001");
+
+    // Deploy ValidatorSet contract
+    //const ValidatorSetFactory = await ethers.getContractFactory("ValidatorSetHbbftMock");
+
+    const ConnectivityTrackerHbbftFactory = await ethers.getContractFactory("ConnectivityTrackerHbbft");
+    
+    //upgrades.prepareUpgrade()
+
+    let connectivityTrackerHbbft = await upgrades.deployProxy(
+        ConnectivityTrackerHbbftFactory,
+        [
+            deployer.address, //address _contractOwner,
+            //address _validatorSetContract,
+            //address _stakingContract,
+            //address _blockRewardContract,
+            //uint256 _minReportAgeBlocks
+        ],
+        { initializer: 'initialize' }
+    ) as ConnectivityTrackerHbbft;
+
+    await connectivityTrackerHbbft.deployed();
+
     // deploy connectivity checker
     // deploy proxy for connectivity checker
+    // initialize connectivity checker
+
     
-    // initialize connectivity checker.
+
+
 
     // upgrade contracts
     // ------------
@@ -14,6 +43,9 @@ async function doUpgrade() {
     // - permission
     // - validator set
     // - staking
+
+
+
 
 }
 
