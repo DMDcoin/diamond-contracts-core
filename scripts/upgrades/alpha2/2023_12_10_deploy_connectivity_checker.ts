a536385ec061d3e16bc56f68be85f2998cabb912
Artsiom Vasilevich <artsiom.vasilevich@aetsoft.net>
2023-12-13 15:06:20 +0300

    connectivity tracker deployment script

--- scripts/upgrades/alpha2/2023_12_10_deploy_connectivity_checker.ts
+++ scripts/upgrades/alpha2/2023_12_10_deploy_connectivity_checker.ts
@@ -1,52 +1,47 @@
 import { ethers, upgrades } from "hardhat";
 import { ConnectivityTrackerHbbft } from "../../../src/types/contracts/ConnectivityTrackerHbbft";
+import { InitialContractsConfiguration } from "../../../tasks/types";
+import { upgradeProxy } from "../upgrades";
 
 
 async function doUpgrade() {
-
     const [deployer] = await ethers.getSigners();
-    //console.log("upgrading with account from: ", deployer.address);
-    //let validatorSetHbbft = await ethers.getContractAt("ValidatorSetHbbft", "0x1000000000000000000000000000000000000001");
+    const minReportAgeBlocks = 10;
+    const timeoutSec = 10;
 
-    // Deploy ValidatorSet contract
-    //const ValidatorSetFactory = await ethers.getContractFactory("ValidatorSetHbbftMock");
+    const contracts = InitialContractsConfiguration.fromFile("initial-contracts.json");
 
+    console.log("executing from account: ", deployer.address);
+
+    for (const conractName of ["ValidatorSetHbbft", "BlockRewardHbbft", "TxPermissionHbbft"]) {
+        console.log("upgrading contract ", conractName);
+        await upgradeProxy(deployer, conractName, contracts.getAddress(conractName)!, timeoutSec);
+    }
+
+    console.log("deploying ConnectivityTracker contract")
     const ConnectivityTrackerHbbftFactory = await ethers.getContractFactory("ConnectivityTrackerHbbft");
-    
-    //upgrades.prepareUpgrade()
-
-    let connectivityTrackerHbbft = await upgrades.deployProxy(
+    const connectivityTrackerHbbft = await upgrades.deployProxy(
         ConnectivityTrackerHbbftFactory,
         [
-            deployer.address, //address _contractOwner,
-            //address _validatorSetContract,
-            //address _stakingContract,
-            //address _blockRewardContract,
-            //uint256 _minReportAgeBlocks
+            deployer.address,                           // address _contractOwner,
+            contracts.getAddress("ValidatorSetHbbft")!, // address _validatorSetContract,
+            contracts.getAddress("StakingHbbft")!,      // address _stakingContract,
+            contracts.getAddress("BlockRewardHbbft")!,  // address _blockRewardContract,
+            minReportAgeBlocks,                         // uint256 _minReportAgeBlocks
         ],
         { initializer: 'initialize' }
     ) as ConnectivityTrackerHbbft;
 
     await connectivityTrackerHbbft.deployed();
 
-    // deploy connectivity checker
-    // deploy proxy for connectivity checker
-    // initialize connectivity checker
+    for (const contractName of ["BlockRewardHbbft", "TxPermissionHbbft"]) {
+        console.log("Set ConnectivityTracker address in ", contractName);
 
-    
+        const factory = await ethers.getContractFactory(contractName);
+        const _contract = factory.attach(contracts.getAddress(contractName)!);
 
-
-
-    // upgrade contracts
-    // ------------
-    // - reward
-    // - permission
-    // - validator set
-    // - staking
-
-
-
-
+        await _contract.connect(deployer).setConnectivityTracker(connectivityTrackerHbbft.address);
+    }
 }
 
 doUpgrade();
--- scripts/upgrades/upgrades.ts
+++ scripts/upgrades/upgrades.ts
@@ -1,14 +1,15 @@
 import { ethers, upgrades } from "hardhat";
+import { getImplementationAddress } from '@openzeppelin/upgrades-core';
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-
-export async function upgradeProxy(contractName: string, proxyAddress: string, timeoutSec: number) {
-    const contractFactory = await ethers.getContractFactory(contractName);
+export async function upgradeProxy(caller: SignerWithAddress, contractName: string, proxyAddress: string, timeoutSec: number) {
+    const contractFactory = await ethers.getContractFactory(contractName, caller);
 
     const contract = await upgrades.upgradeProxy(proxyAddress, contractFactory);
 
     await new Promise((r) => setTimeout(r, timeoutSec * 1000));
 
-    const newImplementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+    const newImplementationAddress = await getImplementationAddress(ethers.provider, proxyAddress);
 
     console.log("Proxy upgraded: ", proxyAddress);
     console.log("New implementation address: ", newImplementationAddress);
