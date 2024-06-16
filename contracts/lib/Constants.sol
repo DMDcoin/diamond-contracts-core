5c0a0de5c3520fe52f9bc33b971b0bbc2ddb3b8f
Thomas Haller <thomashaller@gmx.at>
2024-06-16 23:19:46 +0200

    removed constant to verify reproducable problem with constants.

--- contracts/lib/Constants.sol
+++ contracts/lib/Constants.sol
@@ -5,5 +5,4 @@ address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
 uint256 constant DEFAULT_GAS_PRICE = 1 gwei;
 uint256 constant DEFAULT_BLOCK_GAS_LIMIT = 100_000_000; // 100 MGas block
 uint256 constant MIN_BLOCK_GAS_LIMIT = 100_000_000;
-
-uint256 constant MIN_VALIDATOR_INACTIVITY_TIME = 1 weeks;
+// uint256 constant MIN_VALIDATOR_INACTIVITY_TIME = 1 weeks;
