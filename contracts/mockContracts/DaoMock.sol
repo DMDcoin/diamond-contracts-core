ec50673e764d6c26c80b261793597db3f5f26cdf
Thomas Haller <thomashaller@gmx.at>
2024-07-22 17:54:27 +0200

    DaoMock: switchPhase() now reverts after being called 4 times, so we also test the catch() path on closing the block

--- contracts/mockContracts/DaoMock.sol
+++ contracts/mockContracts/DaoMock.sol
@@ -7,9 +7,17 @@ import { IGovernancePot } from "../interfaces/IGovernancePot.sol";
 contract DaoMock is IGovernancePot {
 
     uint256 phaseCounter;
+    error SwitchPhaseReverted();
 
     function switchPhase() external {
         phaseCounter += 1;
+
+        // in order to also test the scenario that the DAO switchPhase() reverts,
+        // and the blocks are still able to get closed,
+        // we revert here.
+        if (phaseCounter > 4) {
+            revert SwitchPhaseReverted();
+        }
     }
 
     receive() external payable {
