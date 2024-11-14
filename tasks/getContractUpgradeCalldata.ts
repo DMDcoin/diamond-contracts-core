2e4b5deed5fafeeb2ca01a070b9599d07df703fa
Axel Müller <gordn.freeman@proton.me>
2024-11-14 22:35:16 +0300

    contracts upgrade prepare

--- tasks/getContractUpgradeCalldata.ts
+++ tasks/getContractUpgradeCalldata.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
 import { task, types } from 'hardhat/config';
 import { attachProxyAdminV5 } from '@openzeppelin/hardhat-upgrades/dist/utils';
@@ -18,35 +19,23 @@ let KnownContracts = new Map<string, string>([
 
 task("getUpgradeCalldata", "Get contract upgrade calldata to use in DAO proposal")
     .addParam("contract", "The core contract address to upgrade")
+    .addOptionalParam("output", "Output file name", undefined, types.string)
     .addOptionalParam("impl", "Address of new core contract implementation", undefined, types.string)
     .addOptionalParam("initFunc", "Initialization or reinitialization function", undefined, types.string)
     .addOptionalVariadicPositionalParam("constructorArgsParams", "Contract constructor arguments.", [])
     .setAction(async (taskArgs, hre) => {
-        const { contract, impl, initFunc, constructorArgsParams } = taskArgs;
+        const { contract, output, impl, initFunc, constructorArgsParams } = taskArgs;
 
         if (!KnownContracts.has(contract)) {
             throw new Error(`${contract} is unknown`);
         }
 
-        await hre.run(TASK_COMPILE);
+        await hre.run("validate-storage", { contract: contract });
 
         const [deployer] = await hre.ethers.getSigners();
 
         const proxyAddress = KnownContracts.get(contract)!;
         const contractFactory = await hre.ethers.getContractFactory(contract, deployer) as ContractFactory;
-
-        console.log("Validating upgrade compatibility")
-        await hre.upgrades.validateImplementation(contractFactory);
-        await hre.upgrades.validateUpgrade(
-            proxyAddress,
-            contractFactory,
-            {
-                unsafeAllowRenames: false,
-                unsafeSkipStorageCheck: false,
-                kind: "transparent",
-            },
-        );
-        console.log("done!")
 
         let implementationAddress: string = impl;
         if (impl == undefined) {
@@ -76,7 +65,43 @@ task("getUpgradeCalldata", "Get contract upgrade calldata to use in DAO proposal
             initCalldata,
         ]);
 
+        if (output != undefined) {
+            const data = `contract: ${contract}\n`
+                + `calldata: ${calldata}\n`
+                + `  target: ${proxyAdminAddress}\n`;
+
+            fs.writeFileSync(output, data, { flag: 'a' });
+        }
+
         console.log("contract:", contract);
         console.log("calldata:", calldata);
         console.log("  target:", proxyAdminAddress);
+    });
+
+task("validate-storage", "Validate contract upgrade storage compatibility")
+    .addParam("contract", "Name of the contract to validate")
+    .setAction(async (taskArgs, hre) => {
+        await hre.run(TASK_COMPILE);
+
+        if (!KnownContracts.has(taskArgs.contract)) {
+            throw new Error(`${taskArgs.contract} is unknown`);
+        }
+
+        const proxyAddress = KnownContracts.get(taskArgs.contract)!;
+        const contractFactory = await hre.ethers.getContractFactory(taskArgs.contract) as ContractFactory;
+
+        console.log("Validating upgrade compatibility of contract ", taskArgs.contract);
+
+        await hre.upgrades.validateImplementation(contractFactory);
+        await hre.upgrades.validateUpgrade(
+            proxyAddress,
+            contractFactory,
+            {
+                unsafeAllowRenames: false,
+                unsafeSkipStorageCheck: false,
+                kind: "transparent",
+            },
+        );
+
+        console.log("done!")
     });
