ec0cb8fb47b16e25bf5cdf7aae6c25c5e90bd038
Axel Müller <gordn.freeman@proton.me>
2024-11-26 20:58:30 +0300

    BlockRewardHbbft cleanup

--- contracts/mockContracts/BlockRewardHbbftMock.sol
+++ contracts/mockContracts/BlockRewardHbbftMock.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity =0.8.25;
 
+import { IConnectivityTrackerHbbft } from "../interfaces/IConnectivityTrackerHbbft.sol";
 import { BlockRewardHbbft } from "../BlockRewardHbbft.sol";
 
 contract BlockRewardHbbftMock is BlockRewardHbbft {
@@ -8,6 +9,10 @@ contract BlockRewardHbbftMock is BlockRewardHbbft {
 
     function sendCoins() external payable {
         return;
+    }
+
+    function setConnectivityTracker(address _connectivityTracker) external {
+        connectivityTracker = IConnectivityTrackerHbbft(_connectivityTracker);
     }
 
     function setGovernanceAddress(address _address) external {
